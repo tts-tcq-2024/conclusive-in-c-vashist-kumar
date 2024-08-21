aacff7314cf5a6f693c37a9aeec1bac28350a1a8
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-08-21 20:14:38 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -47,4 +47,25 @@ void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double
       break;
   }
 }
+
+void sendToController(BreachType breachType) {
+  const unsigned short header = 0xfeed;
+  printf("%x : %x\n", header, breachType);
+}
+
+void sendToEmail(BreachType breachType) {
+  const char* recepient = "a.b@c.com";
+  switch(breachType) {
+    case TOO_LOW:
+      printf("To: %s\n", recepient);
+      printf("Hi, the temperature is too low\n");
+      break;
+    case TOO_HIGH:
+      printf("To: %s\n", recepient);
+      printf("Hi, the temperature is too high\n");
+      break;
+    case NORMAL:
+      break;
+  }
+
 }
