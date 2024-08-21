46ca97405109f441f1d73065d75db04c2f8caff3
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-08-21 21:38:30 +0530

    Update printMessage.h

--- printMessage.h
+++ printMessage.h
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-void printMsg(const char *message,const char* *message) {
+void printMsg(const char *message) {
     printf("%s\n", message);
+}
+void printtoMsg(const char *message) {
+    printf("To: %s\n", message);
 }
 
 void (*printMessage)(const char *message) = printMsg;
+void (*printToMessage)(const char *message) = printtoMsg;
