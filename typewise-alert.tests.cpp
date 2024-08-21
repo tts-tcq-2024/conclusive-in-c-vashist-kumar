533d70ce86fc4730925005620caa5b227e2903e6
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-08-21 22:49:58 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,10 +1,10 @@
 #include <gtest/gtest.h>
 #include "typewise-alert.h"
 
+
 TEST(TypeWiseAlertTestSuite, test_alert_low_breach) {
   BatteryCharacter batteryChar = {PASSIVE_COOLING," "};
   BreachType expected_breach = TOO_LOW;
   checkAndAlert(TO_CONTROLLER,batteryChar,-10);
-  // ASSERT_EQ(breachType ,expected_breach);
-
+  ASSERT_EQ(,expected_breach)
 }
