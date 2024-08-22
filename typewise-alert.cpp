073f6a7c05778b103d504135dec79a9677bbcc32
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-08-22 11:40:52 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,4 +1,4 @@
-#include "typewise-alertUtils.h"
+
 #include "printMessage.h"
 #include <stdio.h>
 
@@ -72,17 +72,4 @@ void sendToEmail(BreachType breachType) {
     }
 }
 
-void sendToControllerMock(BreachType breachType) {
-  const unsigned short headerMock = 0xfeed;
-  printtocontroller(headerMock, breachType);
-}
 
-void sendToEmailMock(BreachType breachType) {
-    const char* recepientMock = "a.b@c.com";
-    const char* messagesMock[] = {"","Hi, the temperature is too low","Hi, the temperature is too high"};
-    
-    if (breachType == TOO_LOW || breachType == TOO_HIGH) {
-        printToMessage(recepientMock);
-        printMessage(messagesMock[breachType]);
-    }
-}
