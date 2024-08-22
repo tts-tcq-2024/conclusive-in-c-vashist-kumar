54ecd94dc0b224c9530758cc2af25b568a68825f
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-08-22 07:33:12 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -37,11 +37,11 @@ TEST(TypeWiseAlertTestSuite, test_alert_low_breach_to_email) {
   ASSERT_EQ(breachTypeMock ,expected_breach);
 }
 
-TEST(TypeWiseAlertTestSuite, test_alert_hi_breach_to_controller_hiactivecooling) {
+TEST(TypeWiseAlertTestSuite, test_alert_normal_to_controller_hiactivecooling) {
   BatteryCharacter batteryChar = {HI_ACTIVE_COOLING," "};
-  BreachType expected_breach = TOO_HIGH;
-  checkAndAlert(TO_CONTROLLER,batteryChar,50);
-  checkAndAlertMock(TO_EMAIL,batteryChar,50);
+  BreachType expected_breach = NORMAL;
+  checkAndAlert(TO_CONTROLLER,batteryChar,25);
+  checkAndAlertMock(TO_EMAIL,batteryChar,25);
   ASSERT_EQ(breachTypeMock ,expected_breach);
 }
 
