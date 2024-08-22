a1cc80b9969cad117607478975ca06178d7854a7
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-08-22 06:57:31 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -2,17 +2,13 @@
 #include "typewise-alert.h"
 
 
-BreachType breachTypeMock;
-BreachType classifyTemperatureBreachMock(CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = lower_limit[coolingType];
-  int upperLimit = upper_limit[coolingType];
-  BreachType breachTypeMock = inferBreach(temperatureInC, lowerLimit, upperLimit);
-  return inferBreach(temperatureInC, lowerLimit, upperLimit);
-}
+extern BreachType breachTypeMock;
+extern void checkAndAlertMock(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
 
 TEST(TypeWiseAlertTestSuite, test_alert_low_breach) {
   BatteryCharacter batteryChar = {PASSIVE_COOLING," "};
   BreachType expected_breach = TOO_LOW;
   checkAndAlert(TO_CONTROLLER,batteryChar,-10);
+  checkAndAlertMock(TO_CONTROLLER,batteryChar,-10);
   ASSERT_EQ(breachTypeMock ,expected_breach);
 }
